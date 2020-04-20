1efb18bc2db27bed9ab1f3d7e8be222c69a52f1d
Mudassir Gadit <mud27747@gmail.com>
2020-04-20 20:47:37 +0500

    Updates

---
+++ precache-manifest.770d5402f666892eafd38c0252c45234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ae85da2ee5166a87be6c8b1bb1413ca",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "61d5eaf2826adfcd08b5",
+    "url": "/monsters-rolodex/static/css/main.73cab9c6.chunk.css"
+  },
+  {
+    "revision": "9d84f5b306774dfc9f1a",
+    "url": "/monsters-rolodex/static/js/2.91e906d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.91e906d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61d5eaf2826adfcd08b5",
+    "url": "/monsters-rolodex/static/js/main.92597d7c.chunk.js"
+  },
+  {
+    "revision": "3b3d63f18db8e96c0d37",
+    "url": "/monsters-rolodex/static/js/runtime-main.503a8114.js"
+  }
+]);
